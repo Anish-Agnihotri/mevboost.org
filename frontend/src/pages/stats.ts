2a40d9177e18bedc0b6820ee75a4da007b9239bf
Anish Agnihotri <contact@anishagnihotri.com>
2022-09-18 11:45:09 -0400

    feat: frontend: expose stats data via API-endpoint

---
+++ frontend/src/pages/stats.ts
@@ -0,0 +1,26 @@
+import Redis from "ioredis"; // Cache
+
+// Payload type
+export type Stats = {
+  total_blocks: number;
+  total_payloads: number;
+  relays: { name: string; value: number; count: number; avg_value: number }[];
+  builders: {
+    pubkey: string;
+    count: number;
+    value: number;
+    avg_value: number;
+    last_relay: string;
+  }[];
+};
+
+// Setup redis
+const redis = new Redis(import.meta.env.REDIS_URL);
+
+export async function get() {
+  // Collect stats from Redis
+  const statsStr: string | null = await redis.get("stats");
+  const stats: Stats = JSON.parse(statsStr ?? "");
+
+  return new Response(JSON.stringify(stats), { status: 200 });
+}
